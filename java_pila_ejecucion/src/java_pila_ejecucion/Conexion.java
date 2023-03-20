7ccf3791e54b5cc029a3b625ea1963e262ed00a3
Fabio <fabioescobarardila@hotmail.com>
2023-03-20 16:54:03 -0500

    Practica - Java Excepciones: Aprende a crear, lanzar y controlar excepciones - 06. Finally & try with resources, commit 3

--- java_pila_ejecucion/src/java_pila_ejecucion/Conexion.java
+++ java_pila_ejecucion/src/java_pila_ejecucion/Conexion.java
@@ -1,6 +1,6 @@
 package java_pila_ejecucion;
 
-public class Conexion {
+public class Conexion implements AutoCloseable{
 
 	public Conexion() {
 		System.out.println("Iniciando Conexión");
@@ -8,12 +8,18 @@ public class Conexion {
 
 	public void leerDatos() {
 		System.out.println("Recibiendo Datos");
-//		throw new IllegalStateException();
+		throw new IllegalStateException();
 
 	}
 
 	public void cerrar() {
 		System.out.println("Cerrando Conexión");
+	}
+
+	@Override
+	public void close() throws Exception {
+		cerrar();
+		
 	}
 
 }
--- java_pila_ejecucion/src/java_pila_ejecucion/TestConexion.java
+++ java_pila_ejecucion/src/java_pila_ejecucion/TestConexion.java
@@ -3,14 +3,28 @@ package java_pila_ejecucion;
 public class TestConexion {
 
 	public static void main(String[] args) {
-		Conexion con = new Conexion();
+				
+		try(Conexion c = new Conexion()){
+			c.leerDatos();
+		}catch(Exception e) {
+			System.out.println("Catched");
+			e.printStackTrace();
+		}
+		
+		/*
 		try {
+			con = new Conexion();
 			con.leerDatos();			
 		} catch (Exception e) {
-			
+			System.out.println("Catch them all!");
 			e.printStackTrace();
 		} finally {
-			con.cerrar();			
+			if(con!=null) {
+				con.cerrar();				
+			}
+			
 		}
+		*/
+		
 	}
 }
