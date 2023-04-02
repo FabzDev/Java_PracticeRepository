d82f5422259f5c57b99633589cfb7a4ea6da32d4
Fabio <fabioescobarardila@hotmail.com>
2023-04-01 19:31:51 -0500

    Projecto Mortage Calculator - Mosh_JavaUltimateMastery - V6.1: Payment Schedule implementado, commit 9

--- Mosh_JavaMastery/src/MortageCalculator_Project/MortageMain.java
+++ Mosh_JavaMastery/src/MortageCalculator_Project/MortageMain.java
@@ -4,34 +4,33 @@ import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class MortageMain {
+	final static int MONTHS_IN_YEAR = 12;
+	final static int PERCENTAGE = 100;
+
 	public static void main(String[] args) {
 		double principal = readNumber("Principal ($1,000 - $1,000,000): ", 1000, 1000000);
 		double anualIntRate = readNumber("\nAnual Interest Rate: ", 1, 30);
 		byte periodYears = (byte) readNumber("\nPeriod (Years): ", 1, 30);
+		printMortgage(principal, anualIntRate, periodYears);
+		getPaymentSchedule(principal, anualIntRate, periodYears);
+	}
 
+	private static void printMortgage(double principal, double anualIntRate, byte periodYears) {
 		double mortgage = calculateMortgage(principal, anualIntRate, periodYears);
-		double totalPaid= mortgage * periodYears * 12;	
+		double totalPaid = mortgage * periodYears * 12;
 		String mortgageFormated = NumberFormat.getCurrencyInstance().format(mortgage);
 		String totalPaidFormated = NumberFormat.getCurrencyInstance().format(totalPaid);
-		
 		System.out.println("\nMORTGAGE");
 		System.out.println("-----------");
 		System.out.println("Monthly Payments: " + mortgageFormated);
 		System.out.println("Total paid: " + totalPaidFormated);
-		System.out.println("\nPAYMENT SCHEDULE");
-		System.out.println("-------------------");
-		
-		getPaymentSchedule(principal, anualIntRate, periodYears);
 	}
 
 	public static double calculateMortgage(double principal, double anualIntRate, double periodYears) {
-		final int MONTHS_IN_YEAR = 12;
-		final int PERCENTAGE = 100;
-
 		double monthIntRate = anualIntRate / MONTHS_IN_YEAR / PERCENTAGE;
 		byte periodMonths = (byte) (periodYears * MONTHS_IN_YEAR);
-
-		double mortgage = principal * monthIntRate * ((Math.pow((1+monthIntRate), periodMonths))/((Math.pow((1+monthIntRate), periodMonths))-1));
+		double mortgage = principal * monthIntRate
+				* ((Math.pow((1 + monthIntRate), periodMonths)) / ((Math.pow((1 + monthIntRate), periodMonths)) - 1));
 		return mortgage;
 
 	}
@@ -48,21 +47,23 @@ public class MortageMain {
 			System.out.println("Enter a number between " + min + " and " + max + "\n");
 		}
 	}
-	
+
 	public static void getPaymentSchedule(double principal, double anualIntRate, byte periodYears) {
-		final int MONTHS_IN_YEAR = 12;
-		final int PERCENTAGE = 100;
 		double monthIntRate = anualIntRate / MONTHS_IN_YEAR / PERCENTAGE;
 		byte periodMonths = (byte) (periodYears * MONTHS_IN_YEAR);
-		byte paidMonths = 0; 
+		short paidMonths = 0;
 		double balance = principal;
-		
+
+		System.out.println("\nPAYMENT SCHEDULE");
+		System.out.println("-------------------");
+
 		while (balance > 0) {
-		balance = principal * ((Math.pow((1+monthIntRate), periodMonths))-(Math.pow((1+monthIntRate), paidMonths)))
-				/ ((Math.pow((1+monthIntRate), periodMonths)) - 1);
-		if (balance > 0)
+			balance = principal
+					* ((Math.pow((1 + monthIntRate), periodMonths)) - (Math.pow((1 + monthIntRate), paidMonths)))
+					/ ((Math.pow((1 + monthIntRate), periodMonths)) - 1);
+
 			System.out.println(NumberFormat.getCurrencyInstance().format(balance));
-		paidMonths ++;
+			paidMonths++;
 		}
 	}
 
