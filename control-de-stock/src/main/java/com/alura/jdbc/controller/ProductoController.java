90b37ed8ce5704c0f4535e122982a4cf3de23a64
Fabio <fabioescobarardila@hotmail.com>
2023-04-14 00:22:10 -0500

    Proyecto Maven, Control de stock

--- control-de-stock/src/main/java/com/alura/jdbc/controller/ProductoController.java
+++ control-de-stock/src/main/java/com/alura/jdbc/controller/ProductoController.java
@@ -1,7 +1,14 @@
 package com.alura.jdbc.controller;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ProductoController {
 
@@ -13,12 +20,36 @@ public class ProductoController {
 		// TODO
 	}
 
-	public List<?> listar() {
-		// TODO
-		return new ArrayList<>();
+	public List<Map <String, String>> listar() throws SQLException {
+		Connection con = DriverManager.getConnection(
+				"jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC",
+				"root",
+				"faeavf44");
+
+		Statement statement = con.createStatement();
+		
+		statement.execute("SELECT ID, NOMBRE, DESCRIPCION, CANTIDAD FROM PRODUCTO");
+		
+		ResultSet resultSet = statement.getResultSet();
+		
+		List<Map <String, String>> result = new ArrayList<>(); 
+
+		while(resultSet.next()) {
+			Map<String, String> fila = new HashMap<>();
+			fila.put("ID", String.valueOf(resultSet.getInt("ID")));
+			fila.put("NOMBRE", resultSet.getString("NOMBRE"));
+			fila.put("DESCRIPCION", resultSet.getString("DESRIPCION"));
+			fila.put("CANTIDAD", String.valueOf(resultSet.getInt("CANTIDAD")));
+			
+			result.add(fila);
+		}
+		
+		con.close();
+		
+		return result;
 	}
 
-    public void guardar(Object producto) {
+	public void guardar(Object producto) {
 		// TODO
 	}
 
