2f4ada6d2efb7a6d97ed2290044f97135d67c76b
Fabio <fabioescobarardila@hotmail.com>
2023-05-01 17:28:26 -0500

    Alura Java y JDBC - commit 8 - implementando try-with-resourcesen todos los metodos para evitar usar .close()

--- control-de-stock/src/main/java/com/alura/jdbc/controller/ProductoController.java
+++ control-de-stock/src/main/java/com/alura/jdbc/controller/ProductoController.java
@@ -9,66 +9,66 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import javax.management.RuntimeErrorException;
-
 import com.alura.jdbc.clases.ConnectionFactory;
 
 public class ProductoController {
 
 	public void modificar(String nombre, String descripcion, Integer cantidad, Integer id) throws SQLException {
-		Connection con = new ConnectionFactory().recuperaConexion();
-		PreparedStatement statement = con
-				.prepareStatement("UPDATE PRODUCTO SET NOMBRE=? , DESCRIPCION= ?, CANTIDAD = ? WHERE ID = ?;");
+		final Connection con = new ConnectionFactory().recuperaConexion();
+		try (con) {
+			final PreparedStatement statement = con
+					.prepareStatement("UPDATE PRODUCTO SET NOMBRE=? , DESCRIPCION= ?, CANTIDAD = ? WHERE ID = ?;");
+			try (statement) {
+				statement.setString(1, nombre);
+				statement.setString(2, descripcion);
+				statement.setInt(3, cantidad);
+				statement.setInt(4, id);
 
-		statement.setString(1, nombre);
-		statement.setString(2, descripcion);
-		statement.setInt(3, cantidad);
-		statement.setInt(4, id);
-
-		statement.execute();
-		System.out.println("ID " + id + " fue actualizado");
-		con.close();
+				statement.execute();
+				System.out.println("ID " + id + " fue actualizado");
+			}
+		}
 	}
 
 	public int eliminar(Integer id) throws SQLException {
-		Connection con = new ConnectionFactory().recuperaConexion();
-		PreparedStatement statement = con.prepareStatement("DELETE FROM PRODUCTO WHERE ID =?");
+		final Connection con = new ConnectionFactory().recuperaConexion();
+		try (con) {
+			final PreparedStatement statement = con.prepareStatement("DELETE FROM PRODUCTO WHERE ID =?");
+			try (statement) {
+				statement.setInt(1, id);
 
-		statement.setInt(1, Integer.valueOf(id));
+				statement.execute();
 
-		statement.execute();
-		return statement.getUpdateCount();
+				return statement.getUpdateCount();
+			}
+		}
 	}
 
 	public List<Map<String, String>> listar() throws SQLException {
-		// 1. crear conexion
-		Connection con = new ConnectionFactory().recuperaConexion();
-		// 2. crear objeto statement
-		Statement statement = con.createStatement();
 
-		// 3. Ejecutar execute
-		statement.execute("SELECT ID, NOMBRE, DESCRIPCION, CANTIDAD FROM PRODUCTO");
+		final Connection con = new ConnectionFactory().recuperaConexion();
+		try (con) {
+			final PreparedStatement statement = con
+					.prepareStatement("SELECT ID, NOMBRE, DESCRIPCION, CANTIDAD FROM PRODUCTO");
+			try (statement) {
+				statement.execute();
+				final ResultSet resultSet = statement.getResultSet();
+				try (resultSet) {
+					List<Map<String, String>> result = new ArrayList<>();
+					while (resultSet.next()) {
+						Map<String, String> fila = new HashMap<>();
+						fila.put("ID", String.valueOf(resultSet.getInt("ID")));
+						fila.put("NOMBRE", resultSet.getString("NOMBRE"));
+						fila.put("DESCRIPCION", resultSet.getString("DESCRIPCION"));
+						fila.put("CANTIDAD", String.valueOf(resultSet.getInt("CANTIDAD")));
 
-		// 4. obtener el resultado de statement.execute
-		ResultSet resultSet = statement.getResultSet();
+						result.add(fila);
+					}
 
-		// 5. iterar el resultado y guardarlo en un nuevo array
-		List<Map<String, String>> result = new ArrayList<>(); // creando array vacio
-
-		while (resultSet.next()) { // iterando el resultado y agregandolo al array vacio previamente creado
-			Map<String, String> fila = new HashMap<>();
-			fila.put("ID", String.valueOf(resultSet.getInt("ID")));
-			fila.put("NOMBRE", resultSet.getString("NOMBRE"));
-			fila.put("DESCRIPCION", resultSet.getString("DESCRIPCION"));
-			fila.put("CANTIDAD", String.valueOf(resultSet.getInt("CANTIDAD")));
-
-			result.add(fila);
+					return result;
+				}
+			}
 		}
-
-		con.close();
-
-		return result;
 	}
 
 	public void guardar(Map<String, String> producto) throws SQLException {
@@ -77,12 +77,14 @@ public class ProductoController {
 		Integer cantidad = Integer.valueOf(producto.get("CANTIDAD")); // 80
 		Integer maxCant = 50;
 
-		try (Connection con = new ConnectionFactory().recuperaConexion()) {
+		final Connection con = new ConnectionFactory().recuperaConexion();
+		try (con) {
 			con.setAutoCommit(false);
 
-			try (PreparedStatement statement = con.prepareStatement(
+			PreparedStatement statement = con.prepareStatement(
 					"INSERT INTO PRODUCTO(nombre, descripcion, cantidad) VALUES(?,?,?)",
-					Statement.RETURN_GENERATED_KEYS)) {
+					Statement.RETURN_GENERATED_KEYS);
+			try (statement) {
 				do {
 					Integer cantFinal = Math.min(cantidad, maxCant);
 					ejecutarRegistroEnGuardar(nombre, descripcion, cantFinal, statement);
