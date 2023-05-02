4ed0441dfeaf65f55aaca86a88ce8dde93c69a5f
Fabio <fabioescobarardila@hotmail.com>
2023-05-02 14:27:18 -0500

    Alura Java y JDBC - commit 9 - implementando DataStructure poolconexion

--- control-de-stock/src/main/java/com/alura/jdbc/clases/ConnectionFactory.java
+++ control-de-stock/src/main/java/com/alura/jdbc/clases/ConnectionFactory.java
@@ -4,11 +4,24 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 public class ConnectionFactory {
+	private DataSource dataSource;
+	
+	public ConnectionFactory() {
+		var pooledDataSource = new ComboPooledDataSource();
+		pooledDataSource.setJdbcUrl("jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC");;
+		pooledDataSource.setUser("root");
+		pooledDataSource.setPassword("faeavf44");
+		
+		this.dataSource = pooledDataSource;
+		
+	}
 
 	public Connection recuperaConexion() throws SQLException {
-		return DriverManager.getConnection(
-				"jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC", "root", "faeavf44");
-
+		return dataSource.getConnection();
 	}
 }
