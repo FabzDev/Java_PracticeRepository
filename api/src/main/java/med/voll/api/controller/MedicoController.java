43766e49eaf2abc3da684837e447fd6efb362eb6
Fabio <fabioescobarardila@hotmail.com>
2023-05-21 11:46:34 -0500

    Spring Boot 3: desarrollar una API Rest en Java - Spring Data JPA - Validaciones implementadas

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,19 +1,27 @@
 package med.voll.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
 import med.voll.api.medico.DatosMedicoRegistro;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
 
 @RestController
 @RequestMapping("/medicos")
 public class MedicoController {
 	
+	@Autowired
+	private MedicoRepository medicoRepository;
+	
 	@PostMapping
-	public void registrarMedico(@RequestBody DatosMedicoRegistro datosMedicoRegistro) {
-		System.out.println("\nMédico registrado\n");
-		System.out.println(datosMedicoRegistro);
+	public void registrarMedico(@RequestBody @Valid DatosMedicoRegistro datosMedicoRegistro) {
+		
+		medicoRepository.save(new Medico(datosMedicoRegistro));
+		
 	}
 }
---
+++ api/src/main/java/med/voll/api/direccion/Direccion.java
@@ -0,0 +1,29 @@
+package med.voll.api.direccion;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Embeddable
+@Getter
+@AllArgsConstructor
+public class Direccion {
+	
+	private String calle;
+	private String numero;
+	private String complemento;
+	private String distrito;
+	private String ciudad;
+	
+	public Direccion() {
+	}
+	
+	public Direccion(DatosDireccion datosDireccion) {
+		this.calle = datosDireccion.calle();
+		this.numero = datosDireccion.numero();
+		this.distrito = datosDireccion.distrito();
+		this.complemento = datosDireccion.complemento();
+		this.ciudad = datosDireccion.ciudad();
+	}
+	 
+}
