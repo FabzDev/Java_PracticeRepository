7291fee26fd25a5057cbb8ef0f12cafc2ee81d17
Fabio <fabioescobarardila@hotmail.com>
2023-06-06 09:57:47 -0500

    Spring Boot 3: desarrollar una API Rest en Java - JSON Web Token (JWT) - commit 2

--- api/src/main/java/med/voll/api/controller/AutenticationController.java
+++ api/src/main/java/med/voll/api/controller/AutenticationController.java
@@ -2,8 +2,9 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.domain.usuarios.DatosAutentificacionUsusario;
-import med.voll.api.domain.usuarios.TokenService;
 import med.voll.api.domain.usuarios.Usuario;
+import med.voll.api.infra.security.DatosJWTtoken;
+import med.voll.api.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -23,11 +24,12 @@ public class AutenticationController {
 
     @Autowired
     private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity autenticarUsuario(@RequestBody @Valid DatosAutentificacionUsusario datosAutentificacionUsusario){
         Authentication authToken = new UsernamePasswordAuthenticationToken(datosAutentificacionUsusario.login(), datosAutentificacionUsusario.password());
         var usuarioAutenticado = authenticationManager.authenticate(authToken);
         var JWTtoken = tokenService.generarToken((Usuario) usuarioAutenticado.getPrincipal());
-        return ResponseEntity.ok(JWTtoken);
+        return ResponseEntity.ok(new DatosJWTtoken(JWTtoken));
     }
 }
