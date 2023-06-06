78d13dcb759fcc0f4e88c9f65ebc20c5f47cb53d
Fabio <fabioescobarardila@hotmail.com>
2023-06-06 17:29:23 -0500

    Spring Boot 3: desarrollar una API Rest en Java - JSON Web Token (JWT) - Autentificacion completada v1.0

--- api/src/main/java/med/voll/api/infra/security/SecurityConfiguration.java
+++ api/src/main/java/med/voll/api/infra/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package med.voll.api.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,6 +11,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.http.HttpMethod.POST;
 
@@ -17,6 +19,8 @@ import static org.springframework.http.HttpMethod.POST;
 @EnableWebSecurity
 public class SecurityConfiguration {
 
+    @Autowired
+    SecurityFilter securityFilter;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.csrf().disable().sessionManagement()
@@ -24,7 +28,11 @@ public class SecurityConfiguration {
                 .and().authorizeHttpRequests()
                 .requestMatchers(POST, "/login")
                 .permitAll()
-                .and().build();
+                .anyRequest()
+                .authenticated()
+                .and()
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 
     @Bean
--- api/src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ api/src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -4,7 +4,12 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import med.voll.api.domain.usuarios.Usuario;
+import med.voll.api.domain.usuarios.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -16,16 +21,21 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Autowired
     private TokenService tokenService;
 
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        var token = request.getHeader("Authorization");
-        if(token == null || token.equals(""))
-            throw new RuntimeException("El token debe tener contenido");
-
-        token = token.replace("Bearer ", "");
-        System.out.println(token);
-        System.out.println(tokenService.getSubject(token));
+        var headerToken = request.getHeader("Authorization");
+        if(headerToken != null){
+            var token = headerToken.replace("Bearer ", "");
+            var nombreUsuario = tokenService.getSubject(token);
+            if (nombreUsuario != null) {
+                Usuario usuarioEncontrado = (Usuario) usuarioRepository.findUserByLogin(nombreUsuario);
+                Authentication authToken = new UsernamePasswordAuthenticationToken(usuarioEncontrado, null, usuarioEncontrado.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
         filterChain.doFilter(request, response);
-
     }
 }
