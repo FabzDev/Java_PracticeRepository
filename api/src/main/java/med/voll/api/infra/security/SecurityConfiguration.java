f6805637b47c51a5481854deed747dcf35d7320e
Fabio <fabioescobarardila@hotmail.com>
2023-06-07 19:54:53 -0500

    Spring Boot 3: desarrollar una API Rest en Java - JSON Web Token (JWT) - Autentificacion completada v1.1

--- api/src/main/java/med/voll/api/infra/security/SecurityConfiguration.java
+++ api/src/main/java/med/voll/api/infra/security/SecurityConfiguration.java
@@ -26,12 +26,9 @@ public class SecurityConfiguration {
         return httpSecurity.csrf().disable().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().authorizeHttpRequests()
-                .requestMatchers(POST, "/login")
-                .permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                .requestMatchers(POST, "/login").permitAll()
+                .anyRequest().authenticated()
+                .and().addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
