120d0241a32738b6dc6fc1719a3c58273ef716b3
Fabio <fabioescobarardila@hotmail.com>
2023-06-09 17:58:17 -0500

    Practica Estructuras de datos - Ejercicio LinkedList: refactorizados metodos addFirst, adLast, deletefirst, deleteLast, contains, indexOf

--- JavaPracticeExcercises/src/com/practice/excercises/moshDesignPatterns/LinkedList.java
+++ JavaPracticeExcercises/src/com/practice/excercises/moshDesignPatterns/LinkedList.java
@@ -1,13 +1,17 @@
 package com.practice.excercises.moshDesignPatterns;
 
 public class LinkedList {
+    private class Node {
+        int value;
+        Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
     Node first;
     Node last;
-
-    public LinkedList(Node first, Node last) {
-        this.first = first;
-        this.last = last;
-    }
 
     public void print() {
         Node temp = first;
@@ -15,16 +19,23 @@ public class LinkedList {
             System.out.print(temp.value + ", ");
             temp = temp.next;
         }
+        System.out.println();
     }
 
-    public void addFirst(Node item) {
-        item.next = first;
-        first = item;
+    public void addFirst(int number) {
+        var node = new Node(number);
+        if (this.first == null)
+            first = last = node;
+        else {
+            node.next = first;
+            first = node;
+        }
     }
 
-    public void addLast(Node item) {
-        last.next = item;
-        last = item;
+    public void addLast(int  number) {
+        Node node = new Node(number);
+        last.next = node;
+        last = node;
     }
 
     public void deleteFirst() {
@@ -55,7 +66,7 @@ public class LinkedList {
 
     public int indexOf(Integer number) {
         Node temp = first;
-        int count=0;
+        int count = 0;
         while (temp != null) {
             if (temp.value == number) {
                 return count;
