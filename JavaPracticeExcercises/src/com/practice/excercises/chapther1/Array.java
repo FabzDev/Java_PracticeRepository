6377b6b8014fe66476943e443903b3656eefac65
Fabio <fabioescobarardila@hotmail.com>
2023-06-09 01:28:32 -0500

    Practica Estructuras de datos - Ejercicio Binary Tree y Array Clase manual (refactor)

--- JavaPracticeExcercises/src/com/practice/excercises/chapther1/Array.java
+++ JavaPracticeExcercises/src/com/practice/excercises/chapther1/Array.java
@@ -29,36 +29,20 @@ public class Array {
             for (int i = 0; i < pointer; i++) {
                 array[i] = tempArray[i];
             }
-            array[pointer] = value;
-        } else {
-            array[pointer] = value;
         }
+        array[pointer] = value;
         pointer++;
     }
 
-    public void removeAt(int at) {
-        if (at + 1 == array.length) { // array {1,2.3,4,5}, pointer= 5 (next 5)
-            int[] tempArray = new int[pointer - 1]; //
-            for (int i = 0; i < pointer - 1; i++) {
-                tempArray[i] = array[i];
-            }
-            array = new int[pointer - 1];
-            for (int i = 0; i < pointer - 1; i++) {
-                array[i] = tempArray[i];
-            }
-        } else {
-            int[] tempArray = new int[pointer];
-            for (int i = 0; i < pointer; i++) {
-                tempArray[i] = array[i];
-            }
-            array = new int[pointer - 1];
-            for (int i = 0; i < at; i++) {
-                array[i] = tempArray[i];
-            }
-            for (int i = at; i < array.length; i++) {
-                array[i] = tempArray[i+1];
-            }
+    public void removeAt(int at) { // array.length 5, pointer 5, at 2
+        for (int i = at; i < pointer-1; i++) {
+            array[i] = array[i+1];
         }
         pointer--;
+        int[] tempArray = new int[pointer];
+        for (int i = 0; i < pointer; i++) {
+            tempArray[i] = array[i];
+        }
+        array = tempArray;
     }
 }
