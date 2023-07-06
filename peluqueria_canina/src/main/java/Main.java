6316138ed7726a33db1d07a52558e4bd1b304cfb
Fabio <fabioescobarardila@hotmail.com>
2023-07-05 22:55:06 -0500

    Practica Java Swing, aprendiendo layouts

--- peluqueria_canina/src/main/java/Main.java
+++ peluqueria_canina/src/main/java/Main.java
@@ -1,9 +1,11 @@
 import igu.practica.Ejercicio1;
 import igu.practica.Ejercicio2;
 import igu.practica.Ejercicio3;
+import igu.practica.programa;
 
 public class Main {
     public static void main(String[] args) {
-        Ejercicio3 instancia = new Ejercicio3();
+        programa instancia = new programa();
+        instancia.setVisible(true);
     }
 }
--- peluqueria_canina/src/main/java/igu/practica/Ejercicio3.java
+++ peluqueria_canina/src/main/java/igu/practica/Ejercicio3.java
@@ -14,6 +14,8 @@ public class Ejercicio3 {
     int rojo = 0;
     int verde = 0;
     int azul = 0;
+
+
     public Ejercicio3() {
         putFrame();
         putPanel();
@@ -33,45 +35,84 @@ public class Ejercicio3 {
     private void putPanel() {
         panel.setSize(frame.getWidth(), frame.getHeight());
         panel.setLayout(null);
-        panel.setBackground(new Color(rojo, verde, azul));
         frame.add(panel);
     }
 
     private void putLabels(){
         label.setBounds(145, 50, 320, 20);
+        label.setText("Color: Rojo: " + rojo + "  Verde: " + verde + "  Azul: " + azul);
         label.setHorizontalAlignment(SwingConstants.CENTER);
         label.setFont(new Font("Inter", 0, 18));
         panel.add(label);
     }
 
     private void putJButtons(){
+        MouseWheelListener alr = new MouseWheelListener() {
+            @Override
+            public void mouseWheelMoved(MouseWheelEvent e) {
+                if(e.getPreciseWheelRotation()>0 && rojo < 255){
+                    rojo++;}
+                else if(e.getPreciseWheelRotation() < 0 && rojo>0){
+                    rojo--;
+                }
+//                System.out.println("rojo: " + rojo);
+                panel.setBackground(new Color(rojo, verde, azul));
+                label.setText("Color: Rojo: " + rojo + "  Verde: " + verde + "  Azul: " + azul);
+            }
+        };
+        MouseWheelListener alv = new MouseWheelListener() {
+            @Override
+            public void mouseWheelMoved(MouseWheelEvent e) {
+                if(e.getPreciseWheelRotation()>0 && verde < 255){
+                    verde++;}
+                else if(e.getPreciseWheelRotation() < 0 && verde>0){
+                    verde--;
+                }
+//                System.out.println("verde: " + verde);
+                panel.setBackground(new Color(rojo, verde, azul));
+                label.setText("Color: Rojo: " + rojo + "  Verde: " + verde + "  Azul: " + azul);
+
+            }
+        };
+        MouseWheelListener ala = new MouseWheelListener() {
+            @Override
+            public void mouseWheelMoved(MouseWheelEvent e) {
+                if(e.getPreciseWheelRotation()>0 && azul < 255){
+                    azul++;}
+                else if(e.getPreciseWheelRotation() < 0 && azul>0){
+                    azul--;
+                }
+//                System.out.println("azul: " + azul);
+                panel.setBackground(new Color(rojo, verde, azul));
+                label.setText("Color: Rojo: " + rojo + "  Verde: " + verde + "  Azul: " + azul);
+
+            }
+        };
+
         btnRojo.setBounds(80, 350, 120, 30);
         btnRojo.setText("Rojo");
         btnRojo.setForeground(Color.RED);
         btnRojo.setFont(new Font("Inter", 0, 20));
-
-        ActionListener test = new ActionListener() {
+        btnRojo.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                btnAzul.removeMouseWheelListener(new MouseWheelListener() {
-                    @Override
-                    public void mouseWheelMoved(MouseWheelEvent e) {
-
-                    }
-                });
+                panel.removeMouseWheelListener(alv);
+                panel.removeMouseWheelListener(ala);
+                panel.addMouseWheelListener(alr);
             }
-             ;
-        };
+        });
         panel.add(btnRojo);
 
         btnVerde.setBounds(240, 350, 120, 30);
-        btnVerde.setText("Azul");
+        btnVerde.setText("Verde");
         btnVerde.setForeground(Color.GREEN);
         btnVerde.setFont(new Font("Inter", 0, 20));
         btnVerde.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                modificarVerde();
+                panel.removeMouseWheelListener(alr);
+                panel.removeMouseWheelListener(ala);
+                panel.addMouseWheelListener(alv);
             }
         });
         panel.add(btnVerde);
@@ -83,53 +124,11 @@ public class Ejercicio3 {
         btnAzul.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                modificarAzul();
+                panel.removeMouseWheelListener(alr);
+                panel.removeMouseWheelListener(alv);
+                panel.addMouseWheelListener(ala);
             }
         });
         panel.add(btnAzul);
     }
-
-    public void modificarRojo(){
-        panel.addMouseWheelListener(new MouseWheelListener() {
-            @Override
-            public void mouseWheelMoved(MouseWheelEvent e) {
-                if(e.getPreciseWheelRotation()>0 && rojo < 255){
-                    rojo++;}
-                    else if(e.getPreciseWheelRotation() < 0 && rojo>0){
-                    rojo--;
-                }
-                System.out.println("rojo: " + rojo);
-            }
-        });
-    }
-
-    public void modificarVerde(){
-        panel.addMouseWheelListener(new MouseWheelListener() {
-            @Override
-            public void mouseWheelMoved(MouseWheelEvent e) {
-                if(e.getPreciseWheelRotation()>0 && verde < 255){
-                    verde++;}
-                else if(e.getPreciseWheelRotation() < 0 && verde>0){
-                    verde--;
-                }
-                System.out.println("verde: " + verde);
-            }
-        });
-    }
-
-    public void modificarAzul(){
-        panel.addMouseWheelListener(new MouseWheelListener() {
-            @Override
-            public void mouseWheelMoved(MouseWheelEvent e) {
-                if(e.getPreciseWheelRotation()>0 && azul < 255){
-                    azul++;}
-                else if(e.getPreciseWheelRotation() < 0 && azul>0){
-                    azul--;
-                }
-                System.out.println("azul: " + azul);
-            }
-        });
-    }
-
-
 }
