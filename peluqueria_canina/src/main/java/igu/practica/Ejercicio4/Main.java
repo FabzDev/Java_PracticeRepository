0be252e56ed7fdfcfb36355901c6c26f0dcbfab6
Fabio <fabioescobarardila@hotmail.com>
2023-07-07 22:13:17 -0500

    Practica Java Swing, JComboBox resuelto

---
+++ peluqueria_canina/src/main/java/igu/practica/Ejercicio4/Main.java
@@ -0,0 +1,12 @@
+package igu.practica.Ejercicio4;
+
+import igu.practica.Ejercicio4.programa;
+
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        programa instancia = new programa();
+
+    }
+}
