5d842f1caceba08f553a73eb9d506add3d7f9e1b
Fabio <fabioescobarardila@hotmail.com>
2023-07-08 23:44:48 -0500

    Practica Java Swing, Iniciando cronometro

--- peluqueria_canina/src/main/java/igu/IguClase.java
+++ peluqueria_canina/src/main/java/igu/IguClase.java
@@ -1,4 +1,11 @@
 package igu;
 
+import igu.practica.Ejercicio5.Cronometro;
+
+import javax.swing.*;
+
 public class IguClase {
+    public static void main(String[] args) {
+        Cronometro c1 = new Cronometro();
+    }
 }
---
+++ peluqueria_canina/src/main/java/igu/practica/Ejercicio5/Cronotime.java
@@ -0,0 +1,10 @@
+package igu.practica.Ejercicio5;
+
+import javax.swing.*;
+
+public class Cronotime {
+    private JPanel rootPanel;
+    private JPanel backgroundPanel;
+    private JPanel leftPanel;
+    private JPanel rightPanel;
+}
