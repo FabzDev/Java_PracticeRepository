9797a8c80c81bc6c6fb1b562f163adcab09c32d7
Fabio <fabioescobarardila@hotmail.com>
2023-07-25 14:19:59 -0500

    Practica JdbcTabla ' completada

---
+++ tablejdbc/src/main/java/Conexion.java
@@ -0,0 +1,24 @@
+
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class Conexion {
+    private String URL = "jdbc:mysql://localhost:3306/tiendajdbc";
+    private String user = "root";
+    private String password = "faeavf44";
+    
+    public Connection getConnection(){
+        Connection conexion = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conexion = (Connection)DriverManager.getConnection(URL, user, password);
+            
+        } catch (Exception e) {
+            System.out.println("Error Conexion, " + e);
+        }
+        return conexion;
+    }
+}
