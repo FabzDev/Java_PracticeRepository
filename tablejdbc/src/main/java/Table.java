c8324aa56ff661ec5ceb6d528441b29886111b10
Fabio <fabioescobarardila@hotmail.com>
2023-08-11 20:40:02 -0500

    Practica JdbcPaises JComboBox

--- tablejdbc/src/main/java/Table.java
+++ tablejdbc/src/main/java/Table.java
@@ -9,7 +9,7 @@ import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
 public class Table extends javax.swing.JFrame {
-
+    
     public Table() {
         initComponents();
     }
